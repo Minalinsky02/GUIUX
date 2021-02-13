a68456ab59b458d20fae03fae9ffacd819c90ce6
Minalinsky02 <74691266+Minalinsky02@users.noreply.github.com>
2021-02-13 21:21:00 +0800

    Finished all the main menu UI

---
+++ Unity/GUIX/Assets/Scripts/HoverOverItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HoverOverItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private GameObject childText = null; //  or make public and drag
+    void Start()
+    {
+        Text text = GetComponentInChildren<Text>();
+        if (text != null)
+        {
+            childText = text.gameObject;
+            childText.SetActive(false);
+        }
+    }
+    public void OnPointerEnter(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        childText.SetActive(true);
+    }
+    public void OnPointerExit(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        childText.SetActive(false);
+    }
+}
